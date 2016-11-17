3a1a210a500f66440237055715b25a16204b1697
Matias Gianesini <matias.rodgian@gmail.com>
2016-11-17 17:14:22 +0000

    Make 'Enviar' not go to /index.html

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 router.post('/contacto', function(req, res) {
   sendMail(req.body);
-  res.redirect('/index.html');
+  /*
+  res.redirect('/index.html');*/
+  res.redirect(req.get('referer'));
 });
 
 module.exports = router;
