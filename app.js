6e1fedc72d66b632c4cf308f57585aa18a9b6256
matgianesini <matias.rodgian@gmail.com>
2017-01-03 18:19:13 -0600

    Fix delorean name (n with tilde)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var livereload = require('livereload');
 
 var index = require('./routes/index');
 
@@ -13,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,7 +41,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var livereload = require('livereload');
 var lrserver = livereload.createServer();
 lrserver.watch(__dirname + "/public");
 
