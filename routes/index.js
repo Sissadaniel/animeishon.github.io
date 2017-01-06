cd91f00afbddb8982fad3e02c187b793b07e9d39
matgianesini <matias.rodgian@gmail.com>
2017-01-06 16:07:37 -0600

    Make all contact forms work & fix styles

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,19 @@ var sendMail = require('../lib/send_mail.js');
 var router = express.Router();
 
 router.post('/contacto', function(req, res) {
-  sendMail(req.body);
-  /*
-  res.redirect('/index.html');*/
+  sendMail('contacto', req.body);
+  console.log('RESPONSE: ', res);
+  res.redirect(req.get('referer'));
+});
+
+router.post('/contacto_landing', function(req, res) {
+  sendMail('contacto_landing', req.body);
+  console.log('RESPONSE: ', res);
+  res.redirect(req.get('referer'));
+});
+
+router.post('/contacto_mobile', function(req, res) {
+  sendMail('contacto_mobile', req.body);
   console.log('RESPONSE: ', res);
   res.redirect(req.get('referer'));
 });
