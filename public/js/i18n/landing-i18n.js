15085637d807300dbdb806799ccfb4757504ba38
matgianesini <matias.rodgian@gmail.com>
2017-01-23 17:06:51 -0600

    Add styling to language switch button

--- public/js/i18n/landing-i18n.js
+++ public/js/i18n/landing-i18n.js
@@ -28,6 +28,75 @@ jQuery(document).ready(function() {
     $('#loc-section-contact-time-fri-desc').text($.i18n('section-contact-time-fri-desc'));
     $('#loc-section-contact-time-weekend').text($.i18n('section-contact-time-weekend'));
     $('#loc-section-contact-time-weekend-desc').text($.i18n('section-contact-time-weekend-desc'));
+    /* SERVICES SECTION */
+    $('#loc-intro-modal-packs').text($.i18n('intro-packs'));
+    $('#loc-intro-modal-bundle1').text($.i18n('intro-modal-bundle1'));
+    $('#loc-intro-modal-bundle1-bullet1').text($.i18n('intro-modal-bundle1-bullet1'));
+    $('#loc-intro-modal-bundle1-bullet2').text($.i18n('intro-modal-bundle1-bullet2'));
+    $('#loc-intro-modal-bundle1-bullet3').text($.i18n('intro-modal-bundle1-bullet3'));
+    $('#loc-intro-modal-bundle1-bullet4').text($.i18n('intro-modal-bundle1-bullet4'));
+    $('#loc-intro-modal-bundle1-bullet5').text($.i18n('intro-modal-bundle1-bullet5'));
+    $('#loc-intro-modal-bundle1-bullet6').text($.i18n('intro-modal-bundle1-bullet6'));
+    $('#loc-intro-modal-bundle1-bullet7').text($.i18n('intro-modal-bundle1-bullet7'));
+    $('#loc-intro-modal-bundle1-bullet8').text($.i18n('intro-modal-bundle1-bullet8'));
+    $('#loc-intro-modal-bundle1-bullet9').text($.i18n('intro-modal-bundle1-bullet9'));
+    $('#loc-intro-modal-bundle1-bullet10').text($.i18n('intro-modal-bundle1-bullet10'));
+    $('#loc-intro-modal-bundle1-bullet11').text($.i18n('intro-modal-bundle1-bullet11'));
+    $('#loc-intro-modal-bundle1-bullet12').text($.i18n('intro-modal-bundle1-bullet12'));
+    $('#loc-intro-modal-bundle2').text($.i18n('intro-modal-bundle2'));
+    $('#loc-intro-modal-bundle2-bullet1').text($.i18n('intro-modal-bundle2-bullet1'));
+    $('#loc-intro-modal-bundle2-bullet2').text($.i18n('intro-modal-bundle2-bullet2'));
+    $('#loc-intro-modal-bundle2-bullet3').text($.i18n('intro-modal-bundle2-bullet3'));
+    $('#loc-intro-modal-bundle2-bullet4').text($.i18n('intro-modal-bundle2-bullet4'));
+    $('#loc-intro-modal-bundle2-bullet5').text($.i18n('intro-modal-bundle2-bullet5'));
+    $('#loc-intro-modal-bundle2-bullet6').text($.i18n('intro-modal-bundle2-bullet6'));
+    $('#loc-intro-modal-bundle2-bullet7').text($.i18n('intro-modal-bundle2-bullet7'));
+    $('#loc-intro-modal-bundle2-bullet8').text($.i18n('intro-modal-bundle2-bullet8'));
+    $('#loc-intro-modal-bundle2-bullet9').text($.i18n('intro-modal-bundle2-bullet9'));
+    $('#loc-intro-modal-bundle2-bullet10').text($.i18n('intro-modal-bundle2-bullet10'));
+    $('#loc-intro-modal-bundle2-bullet11').text($.i18n('intro-modal-bundle2-bullet11'));
+    $('#loc-intro-modal-bundle2-bullet12').text($.i18n('intro-modal-bundle2-bullet12'));
+    $('#loc-intro-modal-bundle3').text($.i18n('intro-modal-bundle3'));
+    $('#loc-intro-modal-bundle3-bullet1').text($.i18n('intro-modal-bundle3-bullet1'));
+    $('#loc-intro-modal-bundle3-bullet2').text($.i18n('intro-modal-bundle3-bullet2'));
+    $('#loc-intro-modal-bundle3-bullet3').text($.i18n('intro-modal-bundle3-bullet3'));
+    $('#loc-intro-modal-bundle3-bullet4').text($.i18n('intro-modal-bundle3-bullet4'));
+    $('#loc-intro-modal-bundle3-bullet5').text($.i18n('intro-modal-bundle3-bullet5'));
+    $('#loc-intro-modal-bundle3-bullet6').text($.i18n('intro-modal-bundle3-bullet6'));
+    $('#loc-intro-modal-bundle3-bullet7').text($.i18n('intro-modal-bundle3-bullet7'));
+    $('#loc-intro-modal-bundle3-bullet8').text($.i18n('intro-modal-bundle3-bullet8'));
+    $('#loc-intro-modal-bundle3-bullet9').text($.i18n('intro-modal-bundle3-bullet9'));
+    $('#loc-intro-modal-bundle3-bullet10').text($.i18n('intro-modal-bundle3-bullet10'));
+    $('#loc-intro-modal-bundle3-bullet11').text($.i18n('intro-modal-bundle3-bullet11'));
+    $('#loc-intro-modal-bundle3-bullet12').text($.i18n('intro-modal-bundle3-bullet12'));
+    $('#loc-intro-modal-bundle3-bullet13').text($.i18n('intro-modal-bundle3-bullet13'));
+    $('#loc-intro-modal-bundle3-bullet14').text($.i18n('intro-modal-bundle3-bullet14'));
+    $('#loc-intro-modal-bundle3-bullet15').text($.i18n('intro-modal-bundle3-bullet15'));
+    $('#loc-intro-modal-bundle3-bullet16').text($.i18n('intro-modal-bundle3-bullet16'));
+    $('#loc-intro-modal-bundle3-bullet17').text($.i18n('intro-modal-bundle3-bullet17'));
+    $('#loc-intro-modal-bundle4').text($.i18n('intro-modal-bundle4'));
+    $('#loc-intro-modal-bundle4-bullet1').text($.i18n('intro-modal-bundle4-bullet1'));
+    $('#loc-intro-modal-bundle4-bullet2').text($.i18n('intro-modal-bundle4-bullet2'));
+    $('#loc-intro-modal-bundle4-bullet3').text($.i18n('intro-modal-bundle4-bullet3'));
+    $('#loc-intro-modal-bundle4-bullet4').text($.i18n('intro-modal-bundle4-bullet4'));
+    $('#loc-intro-modal-bundle4-bullet5').text($.i18n('intro-modal-bundle4-bullet5'));
+    $('#loc-intro-modal-bundle4-bullet6').text($.i18n('intro-modal-bundle4-bullet6'));
+    $('#loc-intro-modal-bundle4-bullet7').text($.i18n('intro-modal-bundle4-bullet7'));
+    $('#loc-intro-modal-bundle4-bullet8').text($.i18n('intro-modal-bundle4-bullet8'));
+    $('#loc-intro-modal-bundle4-bullet9').text($.i18n('intro-modal-bundle4-bullet9'));
+    $('#loc-intro-modal-bundle4-bullet10').text($.i18n('intro-modal-bundle4-bullet10'));
+    $('#loc-intro-modal-bundle4-bullet11').text($.i18n('intro-modal-bundle4-bullet11'));
+    $('#loc-intro-modal-bundle4-bullet12').text($.i18n('intro-modal-bundle4-bullet12'));
+    $('#loc-intro-modal-bundle4-bullet13').text($.i18n('intro-modal-bundle4-bullet13'));
+    $('#loc-intro-modal-bundle4-bullet14').text($.i18n('intro-modal-bundle4-bullet14'));
+    $('#loc-intro-modal-bundle4-bullet15').text($.i18n('intro-modal-bundle4-bullet15'));
+    $('#loc-intro-modal-bundle4-bullet16').text($.i18n('intro-modal-bundle4-bullet16'));
+    $('#loc-intro-modal-bundle4-bullet17').text($.i18n('intro-modal-bundle4-bullet17'));
+    $('#loc-intro-modal-quote1').text($.i18n('intro-modal-quote'));
+    $('#loc-intro-modal-quote2').text($.i18n('intro-modal-quote'));
+    $('#loc-intro-modal-quote3').text($.i18n('intro-modal-quote'));
+    $('#loc-intro-modal-quote4').text($.i18n('intro-modal-quote'));
+
     /* FOOTER */
     $('#loc-footer-about').text($.i18n('footer-about'));
     $('#loc-footer-whyus').text($.i18n('footer-whyus'));
@@ -74,6 +143,69 @@ jQuery(document).ready(function() {
       'section-contact-time-fri-desc': 'Do you really work on Fridays?',
       'section-contact-time-weekend': 'Saturday and Sunday:',
       'section-contact-time-weekend-desc': 'Don\'t even try it',
+      /* SERVICES */
+      'intro-packs': 'Servicios Web',
+      'intro-modal-quote': 'Cotiza',
+      'intro-modal-bundle1': 'Personal',
+      'intro-modal-bundle1-bullet1': 'Up to 5  static pages',
+      'intro-modal-bundle1-bullet2': 'Responsive design (smartphones, tablets & desktops)',
+      'intro-modal-bundle1-bullet3': '100% custom design',
+      'intro-modal-bundle1-bullet4': 'Free web hosting for 1 year',
+      'intro-modal-bundle1-bullet12': 'Up to 20 @your.domain mail accounts',
+      'intro-modal-bundle1-bullet10': 'Up to 5 contact forms',
+      'intro-modal-bundle1-bullet11': 'Google Analytics integration',
+      'intro-modal-bundle1-bullet5': 'Free changes to banners, images & texts for 1 year',
+      'intro-modal-bundle1-bullet6': 'Free domain registration',
+      'intro-modal-bundle1-bullet7': 'Código optimizado haciendo que el sitio sea lo más veloz posible',
+      'intro-modal-bundle1-bullet7': 'Optimized code making the page load faster',
+      'intro-modal-bundle1-bullet8': '1.5GB Storage',
+      'intro-modal-bundle1-bullet9': '2 rounds of changes to the site\'s structure & looks before launch',
+      'intro-modal-bundle2': 'Negocio',
+      'intro-modal-bundle2-bullet1': 'Up to 15 static pages',
+      'intro-modal-bundle2-bullet2': 'Responsive design (smartphones, tablets & desktops)',
+      'intro-modal-bundle2-bullet3': '100% custom design',
+      'intro-modal-bundle2-bullet4': 'Free web hosting for 1 year',
+      'intro-modal-bundle2-bullet12': 'Unlimited @your.domain mail accounts',
+      'intro-modal-bundle2-bullet10': 'Unlimited contact forms',
+      'intro-modal-bundle2-bullet11': 'Google Analytics integration',
+      'intro-modal-bundle2-bullet5': 'Free changes to banners, images & texts for 1 year',
+      'intro-modal-bundle2-bullet6': 'Free domain registration',
+      'intro-modal-bundle2-bullet7': 'Optimized code making the page load faster',
+      'intro-modal-bundle2-bullet8': '1.5GB Storage',
+      'intro-modal-bundle2-bullet9': '2 rounds of changes to the site\'s structure & looks before launch',
+      'intro-modal-bundle3': 'Página editable',
+      'intro-modal-bundle3-bullet1': 'Up to 5 static pages',
+      'intro-modal-bundle3-bullet13': 'Content Management System',
+      'intro-modal-bundle3-bullet2': 'Responsive design (smartphones, tablets & desktops)',
+      'intro-modal-bundle3-bullet3': '100% custom design',
+      'intro-modal-bundle3-bullet4': 'Free web hosting for 1 year',
+      'intro-modal-bundle3-bullet12': 'Unlimited @your.domain mail accounts',
+      'intro-modal-bundle3-bullet10': 'Unlimited contact forms',
+      'intro-modal-bundle3-bullet11': 'Google Analytics integration',
+      'intro-modal-bundle3-bullet5': '24/7 Technical support for 1 year',
+      'intro-modal-bundle3-bullet6': 'Free domain registration',
+      'intro-modal-bundle3-bullet7': 'Optimized code making the page load faster',
+      'intro-modal-bundle3-bullet8': '1.5GB storage',
+      'intro-modal-bundle3-bullet14': 'User manual',
+      'intro-modal-bundle3-bullet9': '2 rounds of changes to the site\'s structure & looks before launch',
+      'intro-modal-bundle4': 'E-commerce',
+      'intro-modal-bundle4-bullet1': 'Up to 5 static pages',
+      'intro-modal-bundle4-bullet13': 'Content Management System',
+      'intro-modal-bundle4-bullet15': 'Digital storefront',
+      'intro-modal-bundle4-bullet16': 'Inventory manager',
+      'intro-modal-bundle4-bullet17': 'Payment processor & PayPal',
+      'intro-modal-bundle4-bullet2': 'Responsive design (smartphones, tablets & desktops)',
+      'intro-modal-bundle4-bullet3': '100% custom design',
+      'intro-modal-bundle4-bullet4': 'Free web hosting for 1 year',
+      'intro-modal-bundle4-bullet12': 'Unlimited @your.domain mail accounts',
+      'intro-modal-bundle4-bullet10': 'Unlimited contact forms',
+      'intro-modal-bundle4-bullet11': 'Google Analytics integration',
+      'intro-modal-bundle4-bullet5': '24/7 Technical support for 1 year',
+      'intro-modal-bundle4-bullet6': 'Free domain registration',
+      'intro-modal-bundle4-bullet7': 'Optimized code making the page load faster',
+      'intro-modal-bundle4-bullet8': '1.5GB storage',
+      'intro-modal-bundle4-bullet14': 'User manual',
+      'intro-modal-bundle4-bullet9': '2 rounds of changes to the site',
       /* FOOTER */
       'footer-about': 'Animeishon Studio is a creative agency focused on digital design and animation. We provide different creative services with a solid technical experience. hello@animeishon.studio',
       'footer-whyus': 'Why us?',
@@ -110,6 +242,68 @@ jQuery(document).ready(function() {
       'section-contact-time-fri-desc': '¿Trabajas los viernes?',
       'section-contact-time-weekend': 'Sábado y Domingo:',
       'section-contact-time-weekend-desc': 'No preguntes',
+      /* SERVICES SECTION */
+      'intro-packs': 'Web services',
+      'intro-modal-quote': 'Cotiza',
+      'intro-modal-bundle1': 'Personal',
+      'intro-modal-bundle1-bullet1': 'Hasta milochomil páginas estáticas',
+      'intro-modal-bundle1-bullet2': 'Diseño adaptable (smartphones, tabletas y computadoras)',
+      'intro-modal-bundle1-bullet3': 'Diseño  100% personalizado',
+      'intro-modal-bundle1-bullet4': '1 año de hospedaje web gratis',
+      'intro-modal-bundle1-bullet12': 'Hasta 20 cuentas de correo @tu.dominio',
+      'intro-modal-bundle1-bullet10': 'Hasta 5 formularios de contacto',
+      'intro-modal-bundle1-bullet11': 'Integración con Google Analytics',
+      'intro-modal-bundle1-bullet5': '1 año de cambios a banners, imágenes, y textos',
+      'intro-modal-bundle1-bullet6': 'Registro de dominio gratis',
+      'intro-modal-bundle1-bullet7': 'Código optimizado haciendo que el sitio sea lo más veloz posible',
+      'intro-modal-bundle1-bullet8': '1.5GB de almacenamiento',
+      'intro-modal-bundle1-bullet9': '2 rondas de cambios al aspecto y estructura del sitio antes de la entrega final',
+      'intro-modal-bundle2': 'Negocio',
+      'intro-modal-bundle2-bullet1': 'Hasta 15 páginas estáticas',
+      'intro-modal-bundle2-bullet2': 'Diseño adaptable (smartphones, tabletas y computadoras)',
+      'intro-modal-bundle2-bullet3': 'Diseño  100% personalizado',
+      'intro-modal-bundle2-bullet4': '1 año de hospedaje web gratis',
+      'intro-modal-bundle2-bullet12': 'Cuentas de correo ilimitadas @tu.dominio, ligadas a Gmail o Outlook',
+      'intro-modal-bundle2-bullet10': 'Todos los formularios de contacto que se requieran',
+      'intro-modal-bundle2-bullet11': 'Integración con Google Analytics',
+      'intro-modal-bundle2-bullet5': '1 año de cambios a banners, imágenes, y textos',
+      'intro-modal-bundle2-bullet6': 'Registro de dominio gratis',
+      'intro-modal-bundle2-bullet7': 'Código optimizado haciendo que el sitio sea lo más veloz posible',
+      'intro-modal-bundle2-bullet8': '1.5GB de almacenamiento',
+      'intro-modal-bundle2-bullet9': '2 rondas de cambios al aspecto y estructura del sitio antes de la entrega final',
+      'intro-modal-bundle3': 'Página editable',
+      'intro-modal-bundle3-bullet1': 'Hasta 5 páginas estáticas',
+      'intro-modal-bundle3-bullet13': 'Editor de contenidos',
+      'intro-modal-bundle3-bullet2': 'Diseño  adaptable (smartphones, tabletas y computadoras)',
+      'intro-modal-bundle3-bullet3': 'Diseño 100% personalizado',
+      'intro-modal-bundle3-bullet4': '1 año de hospedaje web gratis',
+      'intro-modal-bundle3-bullet12': 'Cuentas de correo ilimitadas @tu.dominio, ligadas a Gmail o Outlook',
+      'intro-modal-bundle3-bullet10': 'Todos los formularios de contacto que se requieran',
+      'intro-modal-bundle3-bullet11': 'Integración con Google Analytics',
+      'intro-modal-bundle3-bullet5': '1 año de soporte técnico y resolución de dudas',
+      'intro-modal-bundle3-bullet6': 'Registro de dominio gratis',
+      'intro-modal-bundle3-bullet7': 'Código optimizado haciendo que el sitio sea lo más veloz posible',
+      'intro-modal-bundle3-bullet8': '1.5GB de almacenamiento',
+      'intro-modal-bundle3-bullet14': 'Manual de usuario',
+      'intro-modal-bundle3-bullet9': '2 rondas de cambios al aspecto y estructura del sitio antes de la entrega final',
+      'intro-modal-bundle4': 'E-commerce',
+      'intro-modal-bundle4-bullet1': 'Hasta 5 páginas estáticas',
+      'intro-modal-bundle4-bullet13': 'Editor de contenidos',
+      'intro-modal-bundle4-bullet15': 'Tienda en linea',
+      'intro-modal-bundle4-bullet16': 'Catálogo y editor de productos',
+      'intro-modal-bundle4-bullet17': 'Pago con tarjeta y PayPal',
+      'intro-modal-bundle4-bullet2': 'Diseño adaptable (smartphones, tabletas y computadoras)',
+      'intro-modal-bundle4-bullet3': 'Diseño 100% personalizado',
+      'intro-modal-bundle4-bullet4': '1 año de hospedaje web gratis',
+      'intro-modal-bundle4-bullet12': 'Cuentas de correo ilimitadas @tu.dominio, ligadas a Gmail o Outlook',
+      'intro-modal-bundle4-bullet10': 'Todos los formularios de contacto que se requieran',
+      'intro-modal-bundle4-bullet11': 'Integración con Google Analytics',
+      'intro-modal-bundle4-bullet5': '1 año de soporte técnico y resolución de dudas',
+      'intro-modal-bundle4-bullet6': 'Registro de dominio gratis',
+      'intro-modal-bundle4-bullet7': 'Código optimizado haciendo que el sitio sea lo más veloz posible',
+      'intro-modal-bundle4-bullet8': '1.5GB de almacenamiento',
+      'intro-modal-bundle4-bullet14': 'Manual de usuario',
+      'intro-modal-bundle4-bullet9': '2 rondas de cambios al aspecto y estructura del sitio antes de la entrega final',
       /* FOOTER */
       'footer-about': 'Animeishon Studio es una agencia especializada en diseño digital y animación. Proveemos distintos servicios enfocados a la creatividad, con fundamentos técnicos sólidos y experiencia. hello@animeishon.studio ',
       'footer-whyus': '¿Por qué nosotros?',
